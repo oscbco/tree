5a98630d459cd1035762ee994f7808c31ca0f631
Oscar Barrera <oscbco@gmail.com>
2019-05-05 18:58:19 -0600

    Integrated react-window into the component.

--- source/Treepane/Treepane.js
+++ source/Treepane/Treepane.js
@@ -1,10 +1,12 @@
 import React from 'react';
+import { FixedSizeList as List } from 'react-window';
+import AutoSizer from 'react-virtualized-auto-sizer';
+
 import { findNode, getNodeByKeypath, getTotal } from '../getFlattenedArrayItem/';
 import css from './_Treepane.scss';
 
 export default function Treepane ({ collection, setCollection, onToggle }) {
   const handleToggle = (keypath) => {
-    console.log(keypath);
     onToggle(keypath);
   };
 
@@ -17,43 +19,43 @@ export default function Treepane ({ collection, setCollection, onToggle }) {
       partialKeypath.push(elem);
       const partialNode = getNodeByKeypath(collection, partialKeypath);
       if (partialKeypath.length === keypath.length) {
-        console.log('node.keypath', keypath);
         return (
-          <React.Fragment>
-            {partialNode.hasNextSibling ? <div className={css.midl}>&nbsp;</div> : <div key={key} className={css.last}>&nbsp;</div>}
-            {partialNode.child ? <div className={css.toggle} data-keypath={[...keypath]} onClick={handleToggle.bind(this, [...keypath])}>{node.toggle ? '+' : '-'}</div> : null}
+          <React.Fragment key={key}>
+            {partialNode.hasNextSibling ? <div className={css.midl}>&nbsp;</div> : <div className={css.last}>&nbsp;</div>}
+            {partialNode.child ? <div className={css.toggle} onClick={handleToggle.bind(this, [...keypath])}>{node.toggle ? '+' : '-'}</div> : null}
           </React.Fragment>
         );
       } else {
-        return partialNode.hasNextSibling ? <div className={css.line}>&nbsp;</div> : <div key={key} className={css.none}>&nbsp;</div>;
+        return partialNode.hasNextSibling ? <div key={key} className={css.line}>&nbsp;</div> : <div key={key} className={css.none}>&nbsp;</div>;
       }
     });
   };
 
-  const printTree = () => {
-    let counter = 0;
-    let node = null;
-    let nodes = [];
-    do {
-      node = findNode({ requestedIndex: counter, collection });
-      counter++;
-      nodes.push(<div className={css.node}>{getTreeLines(node.keypath, node)}{node.value}</div>);
-    } while (node.keypath !== undefined);
-    return nodes;
+  const Row = ({ index, style }) => {
+    const node = findNode({ requestedIndex: index, collection });
+    return (
+      <div style={style} className={css.node}>
+        {getTreeLines(node.keypath, node)}<div className={css.nodeText}>{node.value}</div>
+      </div>
+    );
   };
 
   return (
     <React.Fragment>
-      { getTotal(collection) } items
-      <div className={css.treepanel}>
-        { printTree() }
-        <div className={css.rootNode}>/</div>
-        <div className={css.node}><div className={css.midl}>&nbsp;</div><div className={css.toggle}>-</div>sbin</div>
-        <div className={css.node}><div className={css.line}>&nbsp;</div><div className={css.midl}>&nbsp;</div><div className={css.toggle}>-</div>usr</div>
-        <div className={css.node}><div className={css.line}>&nbsp;</div><div className={css.line}>&nbsp;</div><div className={css.last}>&nbsp;</div><div className={css.toggle}>-</div>local</div>
-        <div className={css.node}><div className={css.line}>&nbsp;</div><div className={css.line}>&nbsp;</div><div className={css.none}>&nbsp;</div><div className={css.last}>&nbsp;</div>www</div>
-        <div className={css.node}><div className={css.line}>&nbsp;</div><div className={css.last}>&nbsp;</div>temp</div><div className={css.node}><div className={css.last}>&nbsp;</div><div className={css.toggle}>+</div>proc</div>
-      </div>
+      <AutoSizer>
+        {({ height, width }) => (
+          <div className={css.treepanel}>
+            <List
+              itemCount={getTotal(collection)}
+              itemSize={20}
+              height={height}
+              width={width}>
+              {Row}
+            </List>
+          </div>
+        )}
+      </AutoSizer>
+
     </React.Fragment>
   );
 }
